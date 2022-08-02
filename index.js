6a6c7b002d135894c9c88615ee7830655ec2c95c
Your Name <you@example.com>
2022-08-01 22:52:04 -0300

    trabalhando com parse

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParses.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/",(req, res) => {
     res.render("index");
 })
 
 app.get("/registrar", (req, res) =>{
-    res.render("registration");
+    var name = req.body.name;
+    var category = req.body.category;
+    var price_sale = req.body.price_sale;
+    var price_cost = req.body.price_cost;
+    var amount = req.body.amount;
+    var date = req.body.date;
 })
 
 app.listen( 8080, () => {});
